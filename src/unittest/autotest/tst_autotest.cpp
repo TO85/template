89088fd6d6eaae745e5f9c8906ac6ce1a9f0a6cf
Rad Eclipser <toor@eagle>
2021-09-07 00:22:44 -0700

    Unit test with separate files for each library (and in real life, even to the class or functionality level)

--- src/unittest/autotest/tst_autotest.cpp
+++ src/unittest/autotest/tst_autotest.cpp
@@ -11,15 +11,16 @@ public:
     autotest();
     ~autotest();
 
+#include "../tplBase/testBase.ih"
+
 private slots:
     void initTestCase();
     void cleanupTestCase();
     void test_case1();
-    void test_tplBase();
 
-private:
-    const tplBase cmBase85 = tplBase(85);
 };
+
+#include "../tplBase/testBase.icpp"
 
 autotest::autotest()
 {
@@ -45,37 +46,8 @@ void autotest::cleanupTestCase()
 void autotest::test_case1()
 {
     QCOMPARE(1+2, 3);
+    QVERIFY(1+1 != 3);
 }
-
-#if 1
-void autotest::test_tplBase()
-{
-    tplBase baseNull;
-    QCOMPARE(0, baseNull.base());
-    QCOMPARE(0, baseNull());
-    QVERIFY(0 == baseNull());
-    QCOMPARE(0, baseNull);
-    QCOMPARE(0, baseNull.sign());
-    QVERIFY(1 != baseNull);
-
-    tplBase baseFortyOne(41);
-    QCOMPARE(41, baseFortyOne);
-    QVERIFY(40 != baseFortyOne());
-    QVERIFY(41 == baseFortyOne());
-    QVERIFY(42 != baseFortyOne());
-    QCOMPARE(baseFortyOne.addition(1), baseFortyOne.addition(1));
-    QCOMPARE(40, baseFortyOne.addition(-1));
-    QCOMPARE(42, baseFortyOne.addition(+1));
-    QCOMPARE(82, baseFortyOne.multiplication(2));
-    QVERIFY(baseFortyOne.add(1) != baseFortyOne.add(1));
-    QCOMPARE(43, baseFortyOne);
-
-    baseFortyOne.sign(-85);
-    QVERIFY(-85 != baseFortyOne);
-    QCOMPARE(-43, baseFortyOne);
-}
-#endif
-
 
 QTEST_APPLESS_MAIN(autotest)
 
--- src/unittest/unittest.pro
+++ src/unittest/unittest.pro
@@ -2,5 +2,4 @@ TEMPLATE = subdirs
 
 SUBDIRS += \
     autotest \
-#    tplBase \
 
